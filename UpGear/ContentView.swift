bbe4f4c79b307a5c271270caf6cab715ca114249
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2025-03-23 18:23:11 -0600

    Initial Commit

---
+++ UpGear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UpGear
+//
+//  Created by Canelito on 23/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UpGear/UpGearApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpGearApp.swift
+//  UpGear
+//
+//  Created by Canelito on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UpGearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
