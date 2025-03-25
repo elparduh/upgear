39f49b6ac2f235d023138a25005148935091a206
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2025-03-25 10:14:48 -0600

    02-Add-LocationDataSource - Data (#2)

---
+++ UpGear/Features/Location/Data/LocationDataSource.swift
@@ -0,0 +1,47 @@
+protocol LocationDataSourceDelegate: AnyObject {
+  func didUpdateLocation(latitude: Double, longitude: Double, speed: Double)
+  func didFailWithError(error: Error)
+  func didDenyPermission()
+}
+
+class LocationDataSource {
+  private let locationManager: LocationManager
+  weak var delegate: LocationDataSourceDelegate?
+
+  init(locationManager: LocationManager = .shared) {
+    self.locationManager = locationManager
+
+    self.locationManager.onLocationUpdate = { [weak self] location in
+      let speed = location.speed >= 0 ? location.speed : 0.0
+      self?.delegate?.didUpdateLocation(
+        latitude: location.coordinate.latitude,
+        longitude: location.coordinate.longitude,
+        speed: speed
+      )
+    }
+
+    self.locationManager.onError = { [weak self] error in
+      self?.delegate?.didFailWithError(error: error)
+    }
+
+    self.locationManager.onPermissionDenied = { [weak self] in
+      self?.delegate?.didDenyPermission()
+    }
+
+    self.locationManager.onPermissionGranted = { [weak self] in
+      self?.startUpdatingLocation()
+    }
+  }
+
+  func requestLocationPermission() {
+    locationManager.requestLocationPermission()
+  }
+
+  func startUpdatingLocation() {
+    locationManager.startUpdatingLocation()
+  }
+
+  func stopUpdatingLocation() {
+    locationManager.stopUpdatingLocation()
+  }
+}
