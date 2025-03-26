35c8b21daa44de730ea4f77761724e93d4e1d2a7
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2025-03-25 21:34:25 -0600

    03-Add-LocationRepository - Data (#3)

---
+++ UpGear/Features/Location/Data/LocationRepository.swift
@@ -0,0 +1,42 @@
+protocol LocationRepositoryDelegate: AnyObject {
+  func didUpdateLocation(latitude: Double, longitude: Double, speed: Double)
+  func didFailWithError(error: Error)
+  func didDenyPermission()
+}
+
+class LocationRepository {
+  private let dataSource: LocationDataSource
+  weak var delegate: LocationRepositoryDelegate?
+
+  init(dataSource: LocationDataSource) {
+    self.dataSource = dataSource
+    self.dataSource.delegate = self
+  }
+
+  func requestLocationPermission() {
+      dataSource.requestLocationPermission()
+  }
+
+  func startTracking() {
+      dataSource.startUpdatingLocation()
+  }
+
+  func stopTracking() {
+      dataSource.stopUpdatingLocation()
+  }
+}
+
+extension LocationRepository : LocationDataSourceDelegate {
+
+  func didUpdateLocation(latitude: Double, longitude: Double, speed: Double) {
+    delegate?.didUpdateLocation(latitude: latitude, longitude: longitude, speed: speed)
+  }
+
+  func didFailWithError(error: any Error) {
+    delegate?.didFailWithError(error: error)
+  }
+
+  func didDenyPermission() {
+    delegate?.didDenyPermission()
+  }
+}
