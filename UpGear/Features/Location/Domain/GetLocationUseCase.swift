7da66720b492e3e66594e60773c11bf25ac96d2a
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2025-03-28 15:53:14 -0600

    add usecase

---
+++ UpGear/Features/Location/Domain/GetLocationUseCase.swift
@@ -0,0 +1,36 @@
+class GetLocationUseCase {
+  private let repository: LocationRepository
+  var onLocationUpdated: ((Double, Double, Double) -> Void)?
+  var onError: ((Error) -> Void)?
+  var onDenyPermission: (() -> Void)?
+
+  init(repository: LocationRepository) {
+    self.repository = repository
+    self.repository.delegate = self
+  }
+
+  func execute() {
+    repository.requestLocationPermission()
+    repository.startTracking()
+  }
+
+  func stop() {
+    repository.stopTracking()
+  }
+}
+
+
+extension GetLocationUseCase : LocationRepositoryDelegate {
+
+  func didUpdateLocation(latitude: Double, longitude: Double, speed: Double) {
+    onLocationUpdated?(latitude, longitude, speed)
+  }
+  
+  func didFailWithError(error: any Error) {
+    onError?(error)
+  }
+  
+  func didDenyPermission() {
+    onDenyPermission?()
+  }
+}
